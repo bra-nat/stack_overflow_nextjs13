b9b2670abeab69d4a179861a1b1711111b6526c9
bra-nat <126584626+bra-nat@users.noreply.github.com>
2023-11-21 00:10:27 +0000

    Layout setup, google font integration, metadat setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,29 @@
 import React from "react";
+import { Inter, Space_Grotesk } from "next/font/google";
+import { Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "DevFlow",
+  description:
+    "Explore DevFlow, a powerful and intuitive Q&A platform for developers, crafted with Next.js. Dive into a community-driven space where developers share knowledge, collaborate on projects, and find solutions to coding challenges. Join the flow, ask questions, and contribute to the ever-growing pool of insights at DevFlow – your go-to hub for seamless development experiences.",
+  icons: {
+    icon: "/assets/images/site-loadGetInitialProps.svg",
+  },
+};
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
 
 export default function RootLayout({
   children,
@@ -9,7 +33,9 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body>{children}</body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
