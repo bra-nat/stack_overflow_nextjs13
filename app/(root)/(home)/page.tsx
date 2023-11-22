c44cf8c04274068abc34627f679d7c07072a6abf
bra-nat <126584626+bra-nat@users.noreply.github.com>
2023-11-22 07:21:43 +0000

    Built the Theme component and implemented dark /light mode functionality

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,12 +1,8 @@
-import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
-      <UserButton afterSignOutUrl="/" />
-      <h1 className="h1-bold">Hello world!</h1>
-      <h2 className="h2-bold">Hello world!</h2>
-      <h3 className="h3-bold">Hello world!</h3>
+
     </div>
   );
 }
