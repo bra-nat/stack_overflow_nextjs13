36b91f3a276da37a28097c7ecf1292ab796364c7
bra-nat <126584626+bra-nat@users.noreply.github.com>
2023-12-19 19:35:58 +0000

    Built the form component in the Questions page

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import Question from '@/components/form/Question';
 
-const AskQuestion = () => {
+const Page = () => {
   return (
-    <div>Ask Question</div>
+    <div>
+    <h1 className='h1-bold text-dark100_light900'>Ask a question</h1>
+    <div className='mt-9'>
+    <Question />
+    </div>
+    </div>
   )
 }
 
-export default AskQuestion;
+export default Page;
