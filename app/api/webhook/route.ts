a06ba72fdc6458a684a94b425932bf771e2193b6
bra-nat <126584626+bra-nat@users.noreply.github.com>
2024-01-16 13:00:05 +0000

    Webhook connection

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -7,9 +7,9 @@ import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
-  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+//   const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
 
-  if (!WEBHOOK_SECRET) {
+  if (!process.env.WEBHOOK_SECRET) {
     throw new Error(
       "Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local"
     );
@@ -33,7 +33,7 @@ export async function POST(req: Request) {
   const body = JSON.stringify(payload);
 
   // Create a new Svix instance with your secret.
-  const wh = new Webhook(WEBHOOK_SECRET);
+  const wh = new Webhook(process.env.WEBHOOK_SECRET);
 
   let evt: WebhookEvent;
 
